244071ab68643fc6dd8a16d6bb7a9d97b859d13c
Bhavithshetty22 <bhavithshetty2203@gmail.com>
2024-10-20 23:20:56 +0530

    Delete main page directory

--- main page/phy.js
+++
@@ -1,9 +0,0 @@
-function toggleSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section.style.display === "block") {
-      section.style.display = "none";
-    } else {
-      section.style.display = "block";
-    }
-  }
-  
